a212468d62a9c430be0cc98e3bfec1453807a054
Edward A Maxedon <edadma@gmail.com>
2025-12-20 20:52:59 -0500

    Fix OTPInput demos - convert function children to proper components
    The Demo component doesn't call function children. Changed demos
    from arrow functions with render props to regular function components.

--- packages/docs/src/components/OTPInputDemo.tsx
+++ packages/docs/src/components/OTPInputDemo.tsx
@@ -4,51 +4,43 @@ import { Demo } from './Demo'
 
 // @example-imports: { OTPInput } from 'asterui'
 // @example-imports: { useState } from 'react'
-export const BasicDemo = () => (
-  <Demo>
-    {() => {
-      // @example-include
-      const [value, setValue] = useState('')
-      // @example-include-end
+export function BasicDemo() {
+  // @example-include
+  const [value, setValue] = useState('')
+  // @example-include-end
 
-      return (
-        <>
-          {/* @example-return */}
-          <OTPInput
-            value={value}
-            onChange={setValue}
-            onComplete={(otp) => console.log('Complete:', otp)}
-          />
-          {/* @example-return-end */}
-        </>
-      )
-    }}
-  </Demo>
-)
+  return (
+    <Demo>
+      {/* @example-return */}
+      <OTPInput
+        value={value}
+        onChange={setValue}
+        onComplete={(otp) => console.log('Complete:', otp)}
+      />
+      {/* @example-return-end */}
+    </Demo>
+  )
+}
 
 // @example-imports: { OTPInput } from 'asterui'
 // @example-imports: { useState } from 'react'
-export const FourDigitDemo = () => (
-  <Demo>
-    {() => {
-      // @example-include
-      const [value, setValue] = useState('')
-      // @example-include-end
+export function FourDigitDemo() {
+  // @example-include
+  const [value, setValue] = useState('')
+  // @example-include-end
 
-      return (
-        <>
-          {/* @example-return */}
-          <OTPInput
-            length={4}
-            value={value}
-            onChange={setValue}
-          />
-          {/* @example-return-end */}
-        </>
-      )
-    }}
-  </Demo>
-)
+  return (
+    <Demo>
+      {/* @example-return */}
+      <OTPInput
+        length={4}
+        value={value}
+        onChange={setValue}
+      />
+      {/* @example-return-end */}
+    </Demo>
+  )
+}
 
 // @example-imports: { OTPInput, Space } from 'asterui'
 export const SizesDemo = () => (
@@ -66,77 +58,65 @@ export const SizesDemo = () => (
 
 // @example-imports: { OTPInput } from 'asterui'
 // @example-imports: { useState } from 'react'
-export const MaskedDemo = () => (
-  <Demo>
-    {() => {
-      // @example-include
-      const [value, setValue] = useState('')
-      // @example-include-end
+export function MaskedDemo() {
+  // @example-include
+  const [value, setValue] = useState('')
+  // @example-include-end
 
-      return (
-        <>
-          {/* @example-return */}
-          <OTPInput
-            value={value}
-            onChange={setValue}
-            mask
-          />
-          {/* @example-return-end */}
-        </>
-      )
-    }}
-  </Demo>
-)
+  return (
+    <Demo>
+      {/* @example-return */}
+      <OTPInput
+        value={value}
+        onChange={setValue}
+        mask
+      />
+      {/* @example-return-end */}
+    </Demo>
+  )
+}
 
 // @example-imports: { OTPInput } from 'asterui'
 // @example-imports: { useState } from 'react'
-export const AlphanumericDemo = () => (
-  <Demo>
-    {() => {
-      // @example-include
-      const [value, setValue] = useState('')
-      // @example-include-end
+export function AlphanumericDemo() {
+  // @example-include
+  const [value, setValue] = useState('')
+  // @example-include-end
 
-      return (
-        <>
-          {/* @example-return */}
-          <OTPInput
-            value={value}
-            onChange={setValue}
-            type="text"
-            length={5}
-          />
-          {/* @example-return-end */}
-        </>
-      )
-    }}
-  </Demo>
-)
+  return (
+    <Demo>
+      {/* @example-return */}
+      <OTPInput
+        value={value}
+        onChange={setValue}
+        type="text"
+        length={5}
+      />
+      {/* @example-return-end */}
+    </Demo>
+  )
+}
 
 // @example-imports: { OTPInput } from 'asterui'
 // @example-imports: { useState } from 'react'
-export const ErrorDemo = () => (
-  <Demo>
-    {() => {
-      // @example-include
-      const [value, setValue] = useState('123')
-      // @example-include-end
+export function ErrorDemo() {
+  // @example-include
+  const [value, setValue] = useState('123')
+  // @example-include-end
 
-      return (
-        <>
-          {/* @example-return */}
-          <OTPInput
-            value={value}
-            onChange={setValue}
-            error
-            length={4}
-          />
-          {/* @example-return-end */}
-        </>
-      )
-    }}
-  </Demo>
-)
+  return (
+    <Demo>
+      {/* @example-return */}
+      <OTPInput
+        value={value}
+        onChange={setValue}
+        error
+        length={4}
+      />
+      {/* @example-return-end */}
+    </Demo>
+  )
+}
 
 // @example-imports: { OTPInput } from 'asterui'
 export const DisabledDemo = () => (
